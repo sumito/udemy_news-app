cd57c3b60731c1651777e014478f55a03c0462b5
sumito <sumito.temma@gmail.com>
2023-01-30 02:14:28 +0900

    #5 make components

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, View,FlatList,SafeAreaView } from 'react-native'
+import ListeItem from './components/ListItem'
+import articles from './dummies/article.json';
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.container}>
+      <FlatList
+      data={articles}
+      renderItem={({ item }) => <ListeItem 
+      imageUrl={item.imageUrl}
+      title={item.title}
+      author={item.author}      
+      /> }
+      keyExtractor={(item,index) => index.toString()}
+      />
+    </SafeAreaView>
+    
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
+  container:{
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
---
+++ components/ListItem.js
@@ -0,0 +1,54 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Image,Text, View } from 'react-native';
+
+
+const ListeItem = ({imageUrl,title,author}) => {
+    return (
+        <View style={styles.ItemContainer}>      
+    
+            <View style={styles.leftContainer}>      
+                <Image 
+                style={{width:100,height:100}}
+                source={{uri:imageUrl}}
+                />  
+            </View>
+            <View style={styles.rightContainer}>        
+                <Text numberOfLines={3} style={styles.text}>{title}</Text>
+                <Text style={styles.subText}>{author}</Text>
+                <StatusBar style="auto" />
+            </View>                
+        </View>                
+    );
+  }
+
+  export default ListeItem;
+  
+
+const styles = StyleSheet.create({
+
+    ItemContainer: {
+      height:100,
+      width:'100%',
+      borderColor:"gray",
+      borderWidth:1,
+      flexDirection:"row",
+    },
+    leftContainer: {
+      width: 100
+    },
+    rightContainer: {
+      flex: 1,
+      flexDirection: 'column',
+      padding: 10,
+      justifyContent: "space-between",
+  
+    },
+    text: {
+      fontsize: 16
+    },
+    subText: {
+      fontSize: 12,
+      color: "gray"
+    }
+});
+
